f9e79032a3750fcbafbc660fefab903aeeb8dec2
chaiqou <lomtadzenikusha@gmail.com>
2023-09-12 12:53:30 +0400

    feat: display image

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,20 @@
     ref="stageRef"
     :style="{ cursor: isDrawing ? 'crosshair' : 'default' }"
   >
-    <v-layer></v-layer>
+    <v-layer>
+      <v-image
+        :config="{
+          image: image,
+        }"
+        :height="windowHeight"
+        :width="windowWidth"
+      />
+    </v-layer>
   </v-stage>
 </template>
 
 <script setup>
-import { ref, reactive, computed } from "vue";
+import { ref, reactive, computed, onMounted } from "vue";
 
 const points = ref([]);
 const cursorMousePosition = ref([0, 0]);
@@ -21,9 +29,21 @@ const isDrawing = ref(false);
 const isEditing = ref(false);
 const floors = ref([]);
 const stageRef = ref();
-const image = reactive({
-  imageObj: new window.Image(),
-  imageUrl: "../src/assets/apartment.png",
+const image = ref(null);
+const windowWidth = ref(0);
+const windowHeight = ref(0);
+
+onMounted(() => {
+  // display image
+  const newImage = new Image();
+  newImage.src = "../src/assets/apartment.png";
+  newImage.onload = () => {
+    image.value = newImage;
+  };
+
+  // Get window coordinants for image
+  windowHeight.value = window.innerHeight;
+  windowWidth.value = window.innerWidth;
 });
 
 const stageConfig = computed(() => ({
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./app.css";
 import VueKonva from "vue-konva";
 
 const app = createApp(App);
