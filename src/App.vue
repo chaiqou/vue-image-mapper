841116f5fc00be04940bedf9f7ab7c21b26fca00
chaiqou <lomtadzenikusha@gmail.com>
2023-09-12 12:26:28 +0400

    feat: Create layer and stage template add all states and handle two method * calculate mouse position with different approach

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,52 @@
-<script setup></script>
+<template>
+  <v-stage
+    :config="stageConfig"
+    @mousedown="handleOnStageClick"
+    @mousemove="handleOnStageMove"
+    ref="stageRef"
+    :style="{ cursor: isDrawing ? 'crosshair' : 'default' }"
+  >
+    <v-layer></v-layer>
+  </v-stage>
+</template>
 
-<template></template>
+<script setup>
+import { ref, reactive, computed } from "vue";
+
+const points = ref([]);
+const cursorMousePosition = ref([0, 0]);
+const isMouseOverStartPoint = ref(false);
+const isFinished = ref(false);
+const isDrawing = ref(false);
+const isEditing = ref(false);
+const floors = ref([]);
+const stageRef = ref();
+const image = reactive({
+  imageObj: new window.Image(),
+  imageUrl: "../src/assets/apartment.png",
+});
+
+const stageConfig = computed(() => ({
+  width: window.innerWidth,
+  height: window.innerHeight,
+}));
+
+const handleOnStageClick = (event) => {
+  const mousePosition = [event.evt.offsetX, event.evt.offsetY];
+
+  if (isEditing.value) {
+    return;
+  }
+
+  if (isMouseOverStartPoint.value && points.value.length >= 3) {
+    isFinished.value = true;
+  } else if (isDrawing.value) {
+    points.value = [...points.value, mousePosition];
+  }
+};
+
+const handleOnStageMove = (event) => {
+  const mousePosition = [event.evt.offsetX, event.evt.offsetY];
+  cursorMousePosition.value = mousePosition;
+};
+</script>
